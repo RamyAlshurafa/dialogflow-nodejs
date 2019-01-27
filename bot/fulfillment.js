a23d0f7f27ebfbece490cf02220a30bda4d75f94
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-01-27 11:56:54 +0200

    debugging trial

--- bot/fulfillment.js
+++ bot/fulfillment.js
@@ -12,19 +12,19 @@ module.exports = (request, response) => {
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
  
-  function welcome(agent) {
-    agent.add(`Welcome to nightingale!`);
-  }
+  // function welcome(agent) {
+  //   agent.add(`Welcome to nightingale!`);
+  // }
   
-    function languages(agent) {
-    agent.add(`you said languages`);
-    agent.add(JSON.stringify(agent));
-  }
+//     function languages(agent) {
+//     agent.add(`you said languages`);
+//     agent.add(JSON.stringify(agent));
+//   }
  
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-}
+//   function fallback(agent) {
+//     agent.add(`I didn't understand`);
+//     agent.add(`I'm sorry, can you try again?`);
+// }
 
   // // Uncomment and edit to make your own intent handler
   // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
@@ -57,9 +57,9 @@ module.exports = (request, response) => {
 
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Languages', languages);
-  intentMap.set('Default Fallback Intent', fallback);
+  // intentMap.set('Default Welcome Intent', welcome);
+  // intentMap.set('Languages', languages);
+  // intentMap.set('Default Fallback Intent', fallback);
   // intentMap.set('your intent name here', yourFunctionHandler);
   // intentMap.set('your intent name here', googleAssistantHandler);
   agent.handleRequest(intentMap);
