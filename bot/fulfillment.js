dd80d8b7bb9c179be74764f9a1f8b8fdb036d59a
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-01-27 13:57:26 +0200

    debugging

--- bot/fulfillment.js
+++ bot/fulfillment.js
@@ -77,7 +77,7 @@ const assistant = dialogflow();
 
 
 assistant.intent('bookingCar', conv => {
-  console.log("+++++++++++++++++++ /////////////////// +++++++++++++++++++ ", conv);
+  console.log("+++++++++++++++++++ /////////////////// +++++++++++++++++++ ", JSON.stringify(conv));
 	// let name = conv.parameters.name;
 	conv.ask('Hello, welcome +++++++++++++++++++++++++++++++++');
 });
