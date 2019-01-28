f1828a95ba9db8ba4a29eb4c5b8129b30330a56d
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-01-28 13:56:15 +0200

    trial

--- bot/fulfillment.js
+++ bot/fulfillment.js
@@ -5,9 +5,16 @@ welcomeFulfillment = (agent) => {
   agent.add("this is another message also")
 }
 
+historyBad = (agent) => {
+  agent.add("this message sent by webhooks for History bad intent")
+  agent.add("this is another message also")
+}
+
 module.exports = (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
   let intentMap = new Map(); 
   intentMap.set('Welcome', welcomeFulfillment);
+  intentMap.set("welcome - yes - good - friends - yes - history no", historyBad);
   agent.handleRequest(intentMap);
 }
+
